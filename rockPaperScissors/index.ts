1ccd7d1c27177e9d08b6f901bfd2efe33975d639
myeongwon <lighting1.front@gmail.com>
2020-05-24 21:35:03 +0900

    Update rockPaperScissors

--- rockPaperScissors/index.ts
+++ rockPaperScissors/index.ts
@@ -1,49 +1,73 @@
-let imgCoords: RSP[keyof RSP] = '0';
-
-interface Example {
-  [key: string]: number; // 어떤것이 들어올지 모를때
-}
-
 interface RSP {
-  readonly ROCK: string,
-  readonly SCISSORS: string,
-  readonly PAPER: string,
+  ROCK: '0';
+  SCISSORS: '-142px';
+  PAPER: '-284px';
 }
 
-const rsp: RSP = {
+let imgCoords: RSP[keyof RSP] = '0';
+const rsp: RSP = { // 딕셔너리 자료구조
   ROCK: '0',
-  SCISSORS: `-142px`,
-  PAPER: `-284px`,
+  SCISSORS: '-142px',
+  PAPER: '-284px'
+}; // as const도 설명
+
+function computerChoice(imgCoords: RSP[keyof RSP]) {
+  return (Object.keys(rsp) as ['ROCK', 'SCISSORS', 'PAPER']).find((k) => {
+    return rsp[k] === imgCoords;
+  });
 }
+
+let interval: number;
+
+function intervalMaker() {
+  interval = setInterval(function () {
+    if (imgCoords === rsp.ROCK) {
+      imgCoords = rsp.SCISSORS;
+    } else if (imgCoords === rsp.SCISSORS) {
+      imgCoords = rsp.PAPER;
+    } else {
+      imgCoords = rsp.ROCK;
+    }
+    if (document.querySelector('#computer')) {
+      (document.querySelector('#computer') as HTMLElement).style.background = 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + imgCoords + ' 0';
+    }
+  }, 100);
+}
+
+intervalMaker();
 
 const score = {
-  ROCK: 0,
   SCISSORS: 1,
+  ROCK: 0,
   PAPER: -1,
-} as const;
-
-// 'ROCK' | 'SCISSORS' | 'PAPER' // union
-function computerChoice(imgCoords: RSP[keyof RSP]): keyof RSP {
-  return (Object.keys(rsp) as ['ROCK', 'SCISSORS', 'PAPER']).find((k) => rsp[k] === imgCoords)!
-  // 튜플
-  // ! 이것은 무조건 값이 있어! (undefined를 없애줄려면)
-  // 안쓰는 방법도 있음
-} 
+};
 
 document.querySelectorAll('.btn').forEach((btn) => {
-  btn.addEventListener('click', function(this: HTMLButtonElement, e: Event){
-    const myChoise = this.textContent as keyof RSP; // as를 사용해 타입을 강제, 정확하게 맞춰준다. 변환해 준다.
-    const myScore = score[myChoise];
-    const computerScore = score[computerChoice(imgCoords)];
-    const diff = myScore - computerScore;
-
-    if(diff === 0 ){
-      console.log('비겼습니다');
-    }else if([-1, 2].indexOf(diff)){
-      console.log('이겼습니다');
-    }else {
-      console.log('졌습니다');
-    }
-
+ btn.addEventListener('click', function (this: HTMLButtonElement) {
+   clearInterval(interval); // setInterval 중지
+   setTimeout(() => {
+     intervalMaker();
+   }, 1000);
+   const myChoice = this.id as keyof RSP;
+   const myScore = score[myChoice];
+   const computerScore = score[computerChoice(imgCoords)!];
+   const diff = myScore - computerScore;
+   if (diff === 0) {
+     console.log('비겼습니다');
+   } else if ([-1, 2].includes(diff)) {
+     console.log('이겼습니다!!');
+   } else {
+     console.log('졌습니다 ㅠㅠ.');
+   }
   });
 });
+
+let start: number = 3;
+const interval2 = setInterval(function() {
+  if (start === 0) {
+    console.log('종료!!!');
+    return clearInterval(interval2);
+  }
+  console.log(start);
+  start -= 1;
+}, 1000);
